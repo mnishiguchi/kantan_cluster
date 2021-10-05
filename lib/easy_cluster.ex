03cd8276a823f643ce711ba2949a5a54226d41eb
Masatoshi N <nishiguchi.masa@gmail.com>
2021-10-05 08:30:24 -0400

    Init

---
+++ lib/easy_cluster.ex
@@ -0,0 +1,18 @@
+defmodule EasyCluster do
+  @moduledoc """
+  Documentation for `EasyCluster`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EasyCluster.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/easy_cluster_test.exs
@@ -0,0 +1,8 @@
+defmodule EasyClusterTest do
+  use ExUnit.Case
+  doctest EasyCluster
+
+  test "greets the world" do
+    assert EasyCluster.hello() == :world
+  end
+end
